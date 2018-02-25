d13c33500336680e927449e2bfc2b39b7075d776
AdamStan <adamstanczyk96@gmail.com>
2018-02-25 23:07:57 +0100

    faculties works

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,17 @@
 package application;
 
+import java.util.ArrayList;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
+import org.hibernate.Transaction;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
+import application.resources.*;
 
 public class Main extends Application {
-	@SuppressWarnings("unused")
 	private static Session session;
 	@Override
 	public void start(Stage primaryStage) {
@@ -33,8 +33,25 @@ public class Main extends Application {
 		SessionFactory factory = HibernateUtil.configureSessionFactory();
         session = factory.openSession();
 	}
+	public static void add_faculties_and_people(){
+		Transaction tx = session.beginTransaction();
+		ArrayList<Faculty> faculties = new ArrayList<Faculty>();
+		faculties.add(new Faculty("EEIA"));
+		faculties.add(new Faculty("MECHANICZNY"));
+		faculties.add(new Faculty("CHEMICZNY"));
+		faculties.add(new Faculty("FTIMS"));
+		faculties.add(new Faculty("BAIS"));
+		faculties.add(new Faculty("WIPOS"));
+		faculties.add(new Faculty("BINOZ"));
+		for(Faculty f : faculties){
+			session.save(f);
+		}
+		tx.commit();
+		System.out.println("Faculties was added");
+	}
 	public static void main(String[] args) {
 		connect_with_database();
+		add_faculties_and_people();
 		launch(args);
 	}
 }

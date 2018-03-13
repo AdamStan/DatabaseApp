62bed583f8011c968acba07601d293e8dd8181cd
AdamStan <adamstanczyk96@gmail.com>
2018-03-13 17:35:41 +0100

    Now every mapping file work how I always wanted

--- src/application/Main.java
+++ src/application/Main.java
@@ -33,7 +33,6 @@ public class Main extends Application {
 		SessionFactory factory = HibernateUtil.configureSessionFactory();
         session = factory.openSession();
 	}
-	@SuppressWarnings("unchecked")
 	public static void add_faculties_and_people(){
 		Transaction tx = session.beginTransaction();
 		ArrayList<Faculty> faculties = new ArrayList<Faculty>();
@@ -44,16 +43,13 @@ public class Main extends Application {
 		faculties.add(new Faculty("BAIS"));
 		faculties.add(new Faculty("WIPOS"));
 		faculties.add(new Faculty("BINOZ"));
-		int number = 200200;
 		for(Faculty f : faculties){
 			session.save(f);
-			Leader leader = new Leader("Jan", "Nowak", f);
-			System.out.println();
-			session.save(leader);
-			Student stud = new Student(number,"Adam","Student", f);
-			session.save(stud);
-			number += 1;
 		}
+		Leader leader = new Leader("Jan", "Nowak", faculties.get(0));
+		session.save(leader);
+		Student stud = new Student(200200,"Adam","Student", faculties.get(0));
+		session.save(stud);
 		tx.commit();
 		System.out.println("Faculties was added");
 	}
--- src/application/view/MainWindowController.java
+++ src/application/view/MainWindowController.java
@@ -1,18 +1,24 @@
 package application.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class MainWindowController {
 	@FXML
 	private TextField usernameField;
 	@FXML
-	private TextField passwordField;
+	private PasswordField passwordField;
 	@FXML
 	public void handleExit(){
 		System.exit(0);
 	}
 	public void handleLogin(){
+		String login = usernameField.getText();
+		String password = passwordField.getText();
 
+		//we have to search table of user...
+		//check theirs roles
+		//load new view
 	}
 }
